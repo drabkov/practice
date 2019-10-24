cfca54a0531aef1fd895ec858c4503382cfa1172
sergey drabkov <sergeydrabkov@gmail.com>
2019-10-24 12:59:54 +0300

    commit

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ func main() {
 	arrayfill.PrintArray(arrayfill.FillArray(arrayfill.CreateArray(5), "1"))
 
 	fmt.Println("task 2 :")
-	m := throwdice.ThrowDice(1000)
+	numberShots := 1000
+	m := throwdice.ThrowDice(numberShots)
 	for i, v := range m {
-		fmt.Printf("%d - %2.1f %%\n", i, float64(v)/float64(1000)*100)
+		fmt.Printf("%d - %2.1f %%\n", i, float64(v)/float64(numberShots)*100)
 	}
 }
