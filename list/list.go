110e1244bac894b5862621513c5cc07ed871e90b
sergey drabkov <sergeydrabkov@gmail.com>
2019-10-30 17:11:17 +0300

    add list

---
+++ list/list.go
@@ -0,0 +1,85 @@
+package list
+
+import "fmt"
+
+type Collection struct {
+	first *Element
+	last  *Element
+	len   int
+}
+
+type Element struct {
+	value int
+	next  *Element
+	prev  *Element
+}
+
+func (e *Element) Next() *Element {
+	return e.next
+}
+
+func (e *Element) Prev() *Element {
+	return e.prev
+}
+
+func (e *Element) Value() int {
+	return e.value
+}
+
+func (c *Collection) First() *Element {
+	return c.first
+}
+
+func (c *Collection) Last() *Element {
+	return c.last
+}
+
+func (c *Collection) Length() int {
+	return c.len
+}
+
+func (c *Collection) Add(element int) {
+	e := &Element{
+		value: element,
+	}
+
+	if c.first == nil {
+		c.first = e
+	} else {
+		e.prev = c.last
+		c.last.next = e
+	}
+	c.last = e
+	c.len++
+}
+
+func (c *Collection) Get(index int) *Element {
+	e := c.first
+	for i := 0; i < index; i++ {
+		e = e.Next()
+	}
+	return e
+}
+
+func (c *Collection) Remove(index int) *Element {
+	e := c.Get(index)
+	if e != nil {
+		e.prev.next = e.next
+		e.next.prev = e.prev
+		e.next = nil
+		e.prev = nil
+		c.len--
+	}
+	return e
+}
+
+func (c *Collection) Print() {
+	fmt.Print("[")
+	for e := c.First(); e != nil; e = e.Next() {
+		fmt.Printf("%d", e.Value())
+		if e.Next() != nil {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Print("]\n")
+}
--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"practice/arrayfill"
+	"practice/list"
+	/*"practice/arrayfill"
 	"practice/throwdice"
-	"sort"
-)
+	"sort"*/)
 
-func main() {
+/*func main() {
 	fmt.Println("task 1 :")
 	arrayfill.PrintArray(arrayfill.FillDiagonals(arrayfill.CreateArray(5, "0"), "1"))
 
@@ -28,4 +28,19 @@ func main() {
 	for _, k := range keys {
 		fmt.Printf("%d - %2.1f %%\n", k, float64(m[k])/float64(numberShots)*100)
 	}
+}*/
+
+func main() {
+	collection := list.Collection{}
+	for i := 0; i < 10; i++ {
+		collection.Add(i)
+	}
+	collection.Print()
+	collection.Remove(4)
+	collection.Print()
+	fmt.Println(collection.Get(4))
+	fmt.Println(collection.First())
+	fmt.Println(collection.Last())
+	fmt.Println(collection.Length())
+	fmt.Println(collection.Get(9).Next())
 }
