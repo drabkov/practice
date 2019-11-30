e7f89d1fe074815030511aaf62f69afb77f2afcb
sergey drabkov <sergeydrabkov@gmail.com>
2019-11-30 22:44:14 +0300

    add dailyweather

---
+++ practice4/mymeteo/dailyweather.go
@@ -0,0 +1,5 @@
+package mymeteo
+
+type DailyWeather struct {
+	
+}
--- practice4/mymeteo/meteorologist.go
+++ practice4/mymeteo/meteorologist.go
@@ -33,62 +33,25 @@ func (m *Meteorologist) WeatherForecast(city string) *Weather {
 	return weather
 }
 
-type Weather struct {
-	Weather []struct {
-		Description string `json:"description"`
-	} `json:"weather"`
-	Main struct {
-		Temp     float64 `json:"temp"`
-		Humidity float64 `json:"humidity"`
-		TempMin  float64 `json:"temp_min"`
-		TempMax  float64 `json:"temp_max"`
-	} `json:"main"`
+func (m *Meteorologist) DailyForecast(city string, cnt int) *DailyWeather {
 
-	Wind struct {
-		Speed float64 `json:"speed"`
-		Deg   float64 `json:"deg"`
-		Gust  float64 `json:"gust"`
-	} `json:"wind"`
-	Sys struct {
-		Sunrise int64 `json:"sunrise"`
-		Sunset  int64 `json:"sunset"`
-	} `json:"sys"`
-}
+	url := "http://api.openweathermap.org/data/2.5/weather?" + "q=" + city + "&lang=ru&units=metric&appid=2c19a8c670afc70f2ae7a81f229fce3d"
 
-func (w *Weather) GetTemperature() (temp float64, tempMin float64, tempMax float64) {
-	return w.Main.Temp, w.Main.TempMin, w.Main.TempMax
-}
-
-func (w *Weather) GetCloudiness() (description string) {
-	return w.Weather[0].Description
-}
-
-func (w *Weather) GetHumidity() (humidity int) {
-	return int(w.Main.Humidity)
-}
-
-func (w *Weather) GetWind() (speed int, gust int, direction string) {
-
-	derection := ""
-	deg := w.Wind.Deg
-	switch {
-	case deg == 0 || deg == 360:
-		derection = "северный"
-	case deg > 0 || deg < 90:
-		derection = "северовосточный"
-	case deg == 90:
-		derection = "восточный"
-	case deg > 90 || deg < 180:
-		derection = "юго-восточный"
-	case deg == 180:
-		derection = "южный"
-	case deg > 180 || deg < 270:
-		derection = "юго-западный"
-	case deg == 270:
-		derection = "западный"
-	case deg > 270 || deg < 360:
-		derection = "северо-западный"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	return int(w.Wind.Speed), gust, derection
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	dw := &DailyWeather{}
+	if err := json.Unmarshal(body, dw); err != nil {
+		fmt.Println(err)
+	}
+
+	return dw
 }
